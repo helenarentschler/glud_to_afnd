7e587aaf1e523d29e2bb4770f698c319205eb382
Helena Rentschler <helrentschler@gmail.com>
2024-06-15 18:48:00 -0300

    primeira versao

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import Form from './components/Form';
 import Automaton from './components/Automaton';
 import Sobre from './components/Sobre';
 import FormalDef from './components/FormalDef';
+import { useState } from 'react';
+import Grammar from './models/Grammar';
+
 function App() {
+
+  const [ grammar, setGrammar ] = useState(null);
+
   return (
     <div className="App">
       <div className="Header">
         <h1>Conversor de Gramática Regular para Autômato - GLUD para AFND-&#949;</h1>
       </div>
       <div className="Container">
-        <Form/>
+        <Form setGrammar={setGrammar}/>
         <FormalDef/>
         <Sobre/> 
       </div>
-      <Automaton/>
+      <Automaton grammar={grammar}/>
     </div>
   );
 }
