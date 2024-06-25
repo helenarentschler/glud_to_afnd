a6e1659768faa42fe6466fcca524dac9756018af
Helena Rentschler <helrentschler@gmail.com>
2024-06-25 16:45:03 -0300

    ajustes css

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -2,21 +2,25 @@
 const Sobre = () => {
     return (
         <div className="sobre">
-            <h2>Sobre</h2>
-            <p>
-                Este Conversor de Gramática Regular para Autômato transforma Gramáticas Lineares Unitárias à Direita (GLUD) 
-                em Autômatos Finitos Não Determinísticos com Transições Vazias (AFND-&#949;). 
-                A ferramenta não realiza a conversão da gramática caso não esteja no formato GLUD, fazendo necessário ao usuário realizar a conversão.
-            </p>
-            <p>
-                O conversor gera um AFND-&#949; equivalente, fornecendo a definição formal e um diagrama de estados.
-            </p>
-            <h3>Orientações</h3>
-            <ul>
-                <li>Espaços em branco são utilizados pelo conversor para diferenciar símbolos terminais de variáveis, portanto, não se deve utilizá-los ao definir um destes símbolos.</li>
-                
-                <li>Separar símbolo terminal de variável utilizando espaço em brancos</li>
-            </ul>
+            <div>
+                 <h2>Sobre</h2>
+                <p>
+                    Este Conversor de Gramática Regular para Autômato transforma Gramáticas Lineares Unitárias à Direita (GLUD) 
+                    em Autômatos Finitos Não Determinísticos com Transições Vazias (AFND-&#949;). 
+                    A ferramenta não realiza a conversão da gramática caso não esteja no formato GLUD, fazendo necessário ao usuário realizar a conversão.
+                </p>
+                <p>
+                    O conversor gera um AFND-&#949; equivalente, fornecendo a definição formal e um diagrama de estados.
+                </p>
+            </div>
+           <div>
+                <h3>Orientações</h3>
+                <ul>
+                    <li>Espaços em branco são utilizados pelo conversor para diferenciar símbolos terminais de variáveis, portanto, não se deve utilizá-los ao definir um destes símbolos.</li>
+                    
+                    <li>Separar símbolo terminal de variável utilizando espaço em brancos</li>
+                </ul>
+           </div>
         </div>
     )
 }
